4e271a42ee922cd5219759808a0f41acb72839b2
Javier <javier_garber@yahoo.es>
2020-05-01 09:30:00 +0200

    pre merge commit

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,4 +1,4 @@
-import Profile from "./profile.container.refactor";
+import Profile from "./profile.container";
 
 import { withTranslation } from "react-i18next";
 export default withTranslation()(Profile);
--- src/containers/Profile/profile.container.js
+++ src/containers/Profile/profile.container.js
@@ -1,8 +1,9 @@
-import React, { Fragment,useState, useEffect, } from 'react';
-import { useTranslation } from 'react-i18next';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { ShexFormBuilder } from '@inrupt/solid-react-components';
-import { successToaster, errorToaster } from '@utils';
+import React, { Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ShexFormBuilder } from "@inrupt/solid-react-components";
+import { successToaster, errorToaster } from "@utils";
+import Provider from "../../services/provider";
+import auth from "solid-auth-client";
 import {
   Header,
   ProfileContainer,
@@ -12,154 +13,163 @@ import {
   WebId,
   SectionProfile,
   Title,
-  Submitdelete
-} from './profile.style';
-import { Image } from './components';
-import {  Provider} from "@services";
+  Submitdelete,
+  RemovePodBtn,
+} from "./profile.style";
+import { Image } from "./components";
 
+const defaultProfilePhoto = "/img/icon/empty-profile.svg";
 
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { webId: props.webId };
+  }
 
+  componentDidMount() {
+    this.getDeleteUrl();
+  }
 
-const defaultProfilePhoto = '/img/icon/empty-profile.svg';
+  async getDeleteUrl() {
+    let value = (await auth.currentSession()).idClaims.iss;
 
-/**
- * We are using ldflex to fetch profile data from a solid pod.
- * ldflex libary is using json-LD for this reason you will see async calls
- * when we want to get a field value, why ? becuase they are expanded the data
- * this means the result will have a better format to read on Javascript.
- * for more information please go to: https://github.com/solid/query-ldflex
- */
+    const provider = Provider.getProviderByBaseURL(value);
 
-const Profile = ({ webId }) => {
-  const { t, i18n } = useTranslation();
-  let state = {value: 'https://solid.community/account/delete'};
+    // If it is not in our providers, we dont know the delete url
+    // so we do not show the delete pod option
+    if (provider) this.setState({ deleteUrl: provider.delete });
+  }
 
-
-
-  const successCallback = () => {
-    successToaster(t('profile.successCallback'), t('profile.successTitle'));
+  sucessCallback = () => {
+    successToaster(
+      this.props.t("profile.successCallback"),
+      this.props.t("profile.successTitle")
+    );
   };
 
-  const errorCallback = e => {
+  errorCallback = (e) => {
     const code = e.code || e.status;
-    const messageError = code ? `profile.errors.${code}` : 'profile.errors.default';
+    const messageError = code
+      ? `profile.errors.${code}`
+      : "profile.errors.default";
     if (code && code !== 200) {
-      errorToaster(t(messageError), 'Error');
+      errorToaster(this.props.t(messageError), "Error");
     }
   };
 
-
-  function  handleChangeSelector(event) {
-    state ={value: event.target.value};
-  }
-  function  handleSubmit(event) {
-     window.open(state.value, "_blank")
+  goDeletURL = (event) => {
+    window.open(this.state.deleteUrl, "_blank");
     event.preventDefault();
+  };
+  render() {
+    const webId = this.props.webId;
+
+    const { t, i18n } = this.props;
+    // const errorCallback = this.errorCallback;
+    // const successCallback = this.sucessCallback;
+
+    console.log("webId", webId);
+    return (
+      <ProfileWrapper data-testid="profile-component">
+        {webId && (
+          <ProfileContainer>
+            <main>
+              <Header>
+                <Image
+                  {...{
+                    webId,
+                    defaultProfilePhoto,
+                  }}
+                />
+                <Title>
+                  <h2>Your PROFILE</h2>
+                </Title>
+              </Header>
+
+              <SectionProfile>
+                <Fragment>
+                  <AutoSaveNotification className="banner-wrap--warning banner">
+                    <div className="banner-wrap__content">
+                      <i className="icon fa fa-exclamation-circle" />
+                      {t("profile.autosaveNotification")}
+                    </div>
+                  </AutoSaveNotification>
+
+                  <ShexForm>
+                    <WebId>
+                      <FontAwesomeIcon icon="id-card" />
+                      <a href={webId} target="_blank" rel="noopener noreferrer">
+                        {webId}
+                      </a>
+                    </WebId>
+
+                    <ShexFormBuilder
+                      {...{
+                        documentUri: webId,
+                        shexUri:
+                          "https://shexshapes.inrupt.net/public/userprofile.shex",
+                        theme: {
+                          form: "shexForm",
+                          shexPanel: "shexPanel",
+                          shexRoot: "shexRoot",
+                          deleteButton:
+                            "deleteButton ids-button-stroke ids-button-stroke--secondary",
+                          inputContainer: "inputContainer",
+                          addButtonStyle:
+                            "addButton ids-button-stroke ids-button-stroke--secondary",
+                        },
+                        languageTheme: {
+                          language: i18n.language.substring(0, 2),
+                          saveBtn: t("profile.saveBtn"),
+                          resetBtn: t("profile.resetBtn"),
+                          addButtonText: t("profile.addBtn"),
+                          deleteButton: t("profile.deleteBtn"),
+                          dropdownDefaultText: t("profile.dropdownDefaultText"),
+                          warningResolution: t("profile.warningResolution"),
+                          formValidate: {
+                            minMxNumberInclusive: t(
+                              "profile.minMxNumberInclusive"
+                            ),
+                            minMxNumberExclusive: t(
+                              "profile.minMxNumberExclusive"
+                            ),
+                            minMaxString: t("profile.minMaxString"),
+                            default: t("profile.defaultError"),
+                          },
+                        },
+                        successCallback: this.sucessCallback,
+                        errorCallback: this.errorCallback,
+                        autoSaveMode: true,
+                      }}
+                    />
+                  </ShexForm>
+                  <br></br>
+                  {/* Show delete btn if we know the delete url */}
+                  {this.state.deleteUrl ? (
+                    <Submitdelete>
+                      {" "}
+                      <div>
+                        {" "}
+                        <br></br>
+                        <p>{t("profile.deletePodMessage")}:</p>{" "}
+                      </div>
+                      <p>
+                        {" "}
+                        <RemovePodBtn
+                          className="deleteButton ids-button-stroke "
+                          onClick={this.goDeletURL}
+                        >
+                          {t("profile.deletePod")}
+                        </RemovePodBtn>{" "}
+                      </p>
+                    </Submitdelete>
+                  ) : null}
+                </Fragment>
+              </SectionProfile>
+            </main>
+          </ProfileContainer>
+        )}
+      </ProfileWrapper>
+    );
   }
-
-  return (
-    <ProfileWrapper data-testid="profile-component">
-      {webId && (
-      <ProfileContainer>
-
-          <main>
-            <Header>
-
-              <Image
-                {...{
-                  webId,
-                  defaultProfilePhoto
-                }}
-              />
-              <Title>
-              <h2>Your PROFILE</h2>
-              </Title>
-            </Header>
-
-            <SectionProfile >
-
-          <Fragment  >
-
-
-            <AutoSaveNotification className="banner-wrap--warning banner">
-              <div className="banner-wrap__content">
-                <i className="icon fa fa-exclamation-circle" />
-                {t('profile.autosaveNotification')}
-              </div>
-            </AutoSaveNotification>
-
-            <ShexForm>
-               <WebId>
-                <FontAwesomeIcon icon="id-card" />
-                <a href={webId} target="_blank" rel="noopener noreferrer">
-                  {webId}
-                </a>
-              </WebId>
-
-
-              <ShexFormBuilder
-                {...{
-                  documentUri: webId,
-                  shexUri: 'https://shexshapes.inrupt.net/public/userprofile.shex',
-                  theme: {
-                    form: 'shexForm',
-                    shexPanel: 'shexPanel',
-                    shexRoot: 'shexRoot',
-                    deleteButton: 'deleteButton ids-button-stroke ids-button-stroke--secondary',
-                    inputContainer: 'inputContainer',
-                    addButtonStyle: 'addButton ids-button-stroke ids-button-stroke--secondary'
-                  },
-                  languageTheme: {
-                    language: i18n.language.substring(0, 2),
-                    saveBtn: t('profile.saveBtn'),
-                    resetBtn: t('profile.resetBtn'),
-                    addButtonText: t('profile.addBtn'),
-                    deleteButton: t('profile.deleteBtn'),
-                    dropdownDefaultText: t('profile.dropdownDefaultText'),
-                    warningResolution: t('profile.warningResolution'),
-                    formValidate: {
-                      minMxNumberInclusive: t('profile.minMxNumberInclusive'),
-                      minMxNumberExclusive: t('profile.minMxNumberExclusive'),
-                      minMaxString: t('profile.minMaxString'),
-                      default: t('profile.defaultError')
-                    }
-                  },
-                  successCallback,
-                  errorCallback,
-                  autoSaveMode: true
-                }}
-              />
-             </ShexForm>
-            <br></br>
-            <Submitdelete> <div> <br></br><p>{t('profile.deletePod') }:</p> </div>
-              <select
-
-                value={ state.value}
-                onChange={handleChangeSelector}
-                onSubmit={ handleSubmit}>
-                {Provider.getIdentityProviders().map((e, key) => {
-                  return (
-                    <option key={key} value={e.delete}>
-                      {e.label}
-                    </option>
-                  );
-                })}
-              </select>
-
-              <p> <button onClick={ handleSubmit}>{t('profile.select')}</button> </p></Submitdelete>
-
-          </Fragment>
-
-            </SectionProfile>
-
-          </main>
-
-
-      </ProfileContainer>
-      )}
-    </ProfileWrapper>
-
-  );
-};
-
-export default Profile;
+}
